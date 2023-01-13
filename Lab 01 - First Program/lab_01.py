c444e9a89fad16092609238668d928f8b2319ce1
Wyatt Lynch <wyguy1121@gmail.com>
2023-01-12 19:57:59 -0500

    added my 4 print statements and finished the code

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-print('Hello')
+print('My name is Wyatt Lynch')
+print('My pizza place rankings: \n1.Little Ceaser\'s\n2.Pizza Hut\n3.Papa John\'s\n4.Dominoes')
+print('\nMy favorite color is red.')
+print('\nI am a fan of the Utah Jazz.')
