d74d2b7eb03f05e3586fa01cc1026ec53cb9df63
Wyatt Lynch <wyguy1121@gmail.com>
2023-02-10 23:33:22 -0500

    finished most of the camel game i think

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,3 +1,6 @@
+import random
+
+
 def main():
     #intro and instructions
     print('Welcome to Camel!')
@@ -11,29 +14,112 @@ def main():
     thirst = 0
     camelTiredness = 0
     distanceNativesTraveled = -20
-    distanceFromNatives = abs(milesTraveled - distanceNativesTraveled)
+    distanceFromNatives = 20
     drinksInCanteen = 3
+
     #main game loop
     while done==False:
-            print('A. Drink from your canteen.')
-            print('B. Ahead moderate speed.')
-            print('C. Ahead full speed.')
-            print('D. Stop for the night.')
-            print('E. Status check.')
-            print('Q. Quit.')
-            userChoice = input("What is your choice? ")
-            userChoice = userChoice.upper()
-            if userChoice == 'Q':
-                done = True
-                print("you have quit the game")
-            elif userChoice == 'E':
-                print("Miles traveled: " + str(milesTraveled))
-                print("Drinks in canteen: " + str(drinksInCanteen))
-                print("The natives are " + str(distanceFromNatives) + " miles behind you")
-            elif userChoice == 'D':
-                camelTiredness = 0
-                print('your camel is happy')
-                
+
+
+        if thirst > 6:
+            print('YOU DIED OF THIRST!')
+            done = True
+            break
+        elif thirst > 4:
+            print('You are thirsty!')
+        if camelTiredness > 8:
+            print('Your camel is dead')
+            done = True
+        elif camelTiredness > 5:
+            print('Your camel is getting tired')
+
+
+
+
+        # calculating distance from the natives
+        if distanceNativesTraveled < 0:
+            distanceFromNatives = abs(distanceNativesTraveled) + milesTraveled
+        else:
+            distanceFromNatives = (milesTraveled - distanceNativesTraveled)
+
+        #check to see if natives are close
+        if distanceFromNatives < 15:
+            print('The natives are getting close!')
+        elif distanceFromNatives < 1:
+            print('The natives caught you!')
+            print('GAME OVER')
+
+
+        #check to see if you won
+        if milesTraveled > 199:
+            print('YOU ESCAPED AND WON THE GAME!')
+            done = True
+
+        #instructions
+        print('A. Drink from your canteen.')
+        print('B. Ahead moderate speed.')
+        print('C. Ahead full speed.')
+        print('D. Stop for the night.')
+        print('E. Status check.')
+        print('Q. Quit.')
+
+        #user selection prompt
+        userChoice = input("What is your choice? ")
+        userChoice = userChoice.upper()
+
+        #selection checks
+        if userChoice == 'Q':
+            done = True
+            print("you have quit the game")
+        elif userChoice == 'E':
+            print("Miles traveled: " + str(milesTraveled))
+            print("Drinks in canteen: " + str(drinksInCanteen))
+            print("The natives are " + str(distanceFromNatives) + " miles behind you")
+        elif userChoice == 'D':
+            camelTiredness = 0
+            print('your camel is happy')
+            distanceNativesTraveled = distanceNativesTraveled + random.randint(7,14)
+
+        #travel selections
+        elif userChoice == 'C':
+            fullSpeedTravelDistance = random.randint(10,20)
+            milesTraveled = milesTraveled + fullSpeedTravelDistance
+            print('You traveled',fullSpeedTravelDistance, 'miles')
+            thirst += 1
+            camelTiredness += random.randint(1,3)
+            distanceNativesTraveled = distanceNativesTraveled + random.randint(7,14)
+
+        elif userChoice == 'B':
+            moderateSpeedTravelDistance = random.randint(5,12)
+            milesTraveled = milesTraveled + moderateSpeedTravelDistance
+            print('You traveld',moderateSpeedTravelDistance,'miles')
+            thirst += 1
+            camelTiredness += 1
+            distanceNativesTraveled = distanceNativesTraveled + random.randint(7, 14)
+
+        
+        elif userChoice == 'A':
+            if drinksInCanteen > 0:
+                drinksInCanteen -= 1
+                thirst = 0
+            else:
+                print('You don\'t have any drinks left in your canteen!')
+
+        #vital checks
+        # if thirst > 6:
+        #     print('YOU DIED OF THIRST!')
+        #     done = True
+        #     break
+        # elif thirst > 4:
+        #     print('You are thirsty!')
+        # if camelTiredness > 8:
+        #     print('Your camel is dead')
+        #     done = True
+        # elif camelTiredness > 5:
+        #     print('Your camel is getting tired')
+
+
+
 
 
 
