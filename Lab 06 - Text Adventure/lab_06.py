0e38c43cde8282b4654ef0c15365d8cff48dcc30
Wyatt Lynch <wyguy1121@gmail.com>
2023-03-04 20:02:53 -0500

    finished homework 6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -10,37 +10,43 @@ class Room:
 def main():
     print('this is main yo')
     room_list = []
+    #ROOM 0
     room = Room("You're in a dark room, you can see the light of a doorway to the east", None, None, 1, None)
     room_list.append(room)
-    room = Room("this room is boring", 3, None, 2, 0)
+    #ROOM 1
+    room = Room("This room is a kitchen, smells like something good is cooking!\nThere are doors North, East, or back the way you came.", 3, None, 2, 0)
     room_list.append(room)
-    room = Room("this room is gay af", None, None, None, 1)
+    #ROOM 2
+    room = Room("This room is a bedroom with something playing on the TV\nThe only door is the one you came in from.", None, None, None, 1)
     room_list.append(room)
-    room = Room("this room is straight witerally", 5, 1, None, None)
+    #ROOM 3
+    room = Room("This looks like it's the living room\nThere is a door to the North, or back the way you came.", 4, 1, None, None)
     room_list.append(room)
-    room = Room("this room swings both ways", None, 3, 6, 4)
+    #ROOM 4
+    room = Room("Woah! this room is a bowling alley.\nYou can go East, West, or back from where you entered.", None, 3, 6, 5)
     room_list.append(room)
-    room = Room("this room wont do anyone", None, None, 5, None)
+    #ROOM 5
+    room = Room("This is just a boring laundry room\nYou can only leave the way you came in.", None, None, 4, None)
     room_list.append(room)
-    room = Room("heeheeheehaw", None, None, None, 5)
+    #ROOM 6
+    room = Room("this is a movie theatre room.\nYou can only leave the way you came in.", None, None, None, 4)
     room_list.append(room)
-    current_room = 0
     done = False
-    
+    current_room = 0
     while done == False:
-        
+
         #printing room description and instructions 
+
         print('')
         print(room_list[current_room].description)
         print('')
-        print("You can go North, South, East, or West")
-        print(current_room)
+        print("You can go North, South, East, or West\nOr you can exit the game.")
         #getting user input and setting it to all caps
         userChoice = input("What direction do you want to go? ")
         userChoice = userChoice.upper()
         
         #If statements to check user input
-        
+
         
         #check if north
         if userChoice == "N" or userChoice == "NORTH":
@@ -76,6 +82,12 @@ def main():
                 print("You can't go that way")
             else: 
                 current_room = nextRoom
+
+        #check if exit
+        elif userChoice == "X" or userChoice == "EXIT":
+            done = True
+
+
         else:
             print("That is not a valid direction")
 
