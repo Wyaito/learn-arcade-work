001c1704e3156e12700d7a84f6c9de0519c194a5
Wyatt Lynch <wyguy1121@gmail.com>
2023-04-07 19:03:51 -0400

    tryna figure it out

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -4,11 +4,23 @@ import re
 # a list of words in the line.
 def split_line(line):
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?',line)
+#create dictionary list
 dictionary = open('dict.txt')
 dict_list = []
 for line in dictionary:
     line = line.strip()
     dict_list.append(line)
 dictionary.close()
+#load alice in wonderland
+AIW = open('AliceInWonderLand200.txt')
+for line in AIW:
+    word_list = []
+    #AIW = open('AliceInWonderLand200.txt')
+    line = split_line(line)
+    word_list.append(line)
+print(word_list)
+
+
+
 print('--- linear search ---')
 
